c714c5b34a9179a4035c3c256903f32f5b54092b
ashe-sleeps <mags1010@icloud.com>
2022-07-08 11:58:40 -0500

    added correct file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,41 @@
+// TODO: Create 4-String variables to introduce develop your story.
+
+var note = "This story is based on the game Minecraft, however its story is original to me"
+var settingBlock = "netherrack"
+var character = "A tall pig-like figure, still mostly humanoid,"
+var mobs = "piglins"
+var overworldSetting = "tundra"
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+
+var mobNum = 5
+var houseNum = 2
+var goldNum = 3
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+
+var items = ["netherite pickaxe", "gold", "crossbows", "golden swords", "cloak", "chest"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+
+var boolTheory = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(note + '.')
+console.log("The hazy heat spreads across the red " + settingBlock + " like a heavy weight, with the bubbling pools of lava below stretching far into the void beyond.")
+console.log(character + " walks across the desolate land dressed in brown trousers, which are much too short for his long legs that end in pig hooves, and a soft cream poet shirt that is cuffed around his elbows.")
+console.log("The figure carries a shiny " + items[0] + " on his shoulder with one hand, the heavy metal seemingly bearing no weight on him, and in his other, he holds up " + goldNum + " shiny pieces of " + items[1] + " to inspect them.")
+console.log("A group of " + mobNum + " " + mobs + " approach him from behind a " + settingBlock + " pillar, but upon seeing his similar pig-like features, they leave him be with a simple nod as they head towards a crimson forest with their " + items[2] + " and " + items[3] + " in hand.")
+console.log("The figure finishes inspecting the " + items[1] + " and places the " + goldNum + " pieces in a draw-string sack on his hip before following the " + mobs + " into the forest.")
+console.log("He weaves in between the red vines and trees and follows narrow " + settingBlock + " paths that cross the massive lakes of lava.")
+console.log("At last, he reaches a swirling obsidian portal and carefully steps through into a " + overworldSetting + " in the overworld.")
+console.log("He shivers at the change in temperature and picks up his " + items[4] + " that he left on this side of the portal.")
+console.log("He slings the " + items[4] + " on with one hand, quickly securing the clasp, and hastily walks through the " + overworldSetting + " to a fenced area where " + houseNum + " white houses jut out from the snow.")
+console.log("He walks up the steps and cracks open the door of the farthest house.")
+console.log("He shouts into the empty room: I'm back with the " + items[1] + " you needed!")
+console.log("A voice further in the house replies: Oh, thank you! You can just put it in a " + items[5] + " somewhere.")
+console.log("As he was emptying his sack into a nearby " + items[5] + ", he remembered something else: You know that theory you had about the " + mobs + "?")
+console.log("The voice replies: Yeah?")
+console.log("He sighs with a slight grin knowing the other had won the their long-running bet: Turns out its " + boolTheory + ", they completely left me alone.")
+console.log("The voice laughs and shouts: I told you it was " + boolTheory + "! Let's go!")
